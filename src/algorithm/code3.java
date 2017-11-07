86d0f7a2e8f7b8a9e844adcc303f6223b840f54d
Reinkaos <ds2lvg@gmail.com>
2017-11-07 19:57:25 +0900

    Recursion과 수학적귀납법

---
+++ src/algorithm/code3.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+// Recursion과 수학적귀납법
+public class code3 {
+	public static void main(String[] args) {
+		int result = factorial(4);
+		System.out.println(result);
+	}
+	public static int factorial(int n) {
+		// n이 0이면 1을 반환
+		if(n==0) 
+			return 1;
+		// (n이 0보다 크다면) 1부터 n까지의 곱은 1에서 n-1까지의 곱에 n을 곱한것
+		return n * factorial(n-1);		
+	}
+}
+
+
