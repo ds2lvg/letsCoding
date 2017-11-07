cca45d31e71ec1d3b6eaf15b5cbad7c58509aa9f
Reinkaos <ds2lvg@gmail.com>
2017-11-07 19:57:43 +0900

    피보나치 수열

---
+++ src/algorithm/code5.java
@@ -0,0 +1,17 @@
+package algorithm;
+
+public class code5 {
+	public static void main(String[] args) {
+		int result = fibonacci(6);
+		System.out.println(result);
+	}
+	// Fibonacci Number
+	public static int fibonacci(int n) {
+		if(n<2) { 
+			return n;
+		}else
+			return fibonacci(n-1) + fibonacci(n-2);		
+	}
+}
+
+
